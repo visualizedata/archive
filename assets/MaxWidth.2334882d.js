82f002912dcd5c170b8eecf881eb72254b61bfeb
danielsauter <contact@daniel-sauter.com>
2022-06-27 21:10:37 +0000

    Deploying to gh-pages from @ visualizedata/archive@36e2cc7d7b94701730bcf7c57d213dcbf301632d 🚀

---
+++ assets/MaxWidth.2334882d.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vue_export-helper.21dcd24c.js";import{o as a,g as r,v as o}from"./vendor.97447cf5.js";const _={};function d(e,n){return a(),r("div",null,[o(e.$slots,"default",{},void 0,!0)])}var l=t(_,[["render",d],["__scopeId","data-v-5937b1a5"]]);export{l as default};
---
+++ assets/image_not_found.71e278d7.js
@@ -0,0 +1,1 @@
+var a="/assets/image_not_found.0c677aea.png";export{a as default};
